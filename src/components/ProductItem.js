b82d76aa8da811a9317bcd7f857773d6fa12ac51
kakimaru <memimei0306@gmail.com>
2024-09-24 07:42:28 +0900

    Added show products function

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -1,7 +1,27 @@
-import { Component } from "../common/Component";
+import { Component } from "../common/Component.js";
 
 export class ProductItem extends Component {
+  constructor(props) {
+    super(props)
+    this.handleAddToCart = this.handleAddToCart.bind(this)
+  }
+
+  handleAddToCart() {
+    this.props.cartContext.addProduct(this.props.productData)
+  }
+
   render() {
-    
+    const product = document.createElement('div')
+    product.innerHTML = `
+      <img src="${this.props.productData.image}" alt="${this.props.productData.title}" class='product-image' />
+      <h3 class='product-title'>${this.props.productData.title}</h3>
+      <p class='product-desc'>${this.props.productData.description}</p>
+      <p>$ ${this.props.productData.price}</p>
+      <button class='product-addBtn'>Add to Cart</button>
+    `
+
+    product.querySelector('.product-addBtn').addEventListener('click', this.handleAddToCart)
+
+    return product
   }
 }
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,8 +1,35 @@
-import { Component } from "../common/Component";
-import { ProductItem } from "./ProductItem";
+import { Component } from "../common/Component.js";
+import { ProductItem } from "./ProductItem.js";
 
 export class ProductList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { products: [] };
+  }
+
+  mount(container) {
+    fetch(`https://fakestoreapi.com/products`)
+      .then((res) => res.json())
+      .then((data) => {
+        this.state.products = data;
+        container.appendChild(this.render());
+      })
+      .catch((err) => console.error(`Error retrieving data:`, err));
+  }
+
   render() {
-    
+    const productList = document.createElement("div");
+    productList.className = "product-list";
+    productList.innerHTML = "";
+
+    this.state.products.forEach((productData) => {
+      const productItem = new ProductItem({
+        productData,
+        cartContext: this.props.cartContext,
+      });
+      productList.appendChild(productItem.render());
+    });
+
+    return productList;
   }
 }
